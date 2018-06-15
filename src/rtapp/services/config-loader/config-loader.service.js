6dfac721045c592cbc8388458fb11247b8de7538
James Gilmore <jamesg1381@gmail.com>
2018-06-15 23:33:51 +0100

    Tidying

--- src/rtapp/services/config-loader/config-loader.service.js
+++ src/rtapp/services/config-loader/config-loader.service.js
@@ -1,5 +1,10 @@
 import configFilePath from 'file-loader?name=[name].[ext]?[hash]!./../../config.json'
 
+const ConfigStorage = {
+  LOCAL_STORAGE:
+    'rt-config'
+}
+
 export default class ConfigLoaderService {
   /* @ngInject */
   constructor ($http, $q, $window) {
@@ -9,15 +14,14 @@ export default class ConfigLoaderService {
   }
   get () {
     let deferred = this.$q.defer()
-    let storedConfig = this.$window.localStorage.getItem('rt-config')
+    let storedConfig = this._getStoredConfig()
 
     if (storedConfig) {
-      let storedConfigObject = JSON.parse(storedConfig)
       if(this.config) {
-        this.config.api = storedConfigObject.api
-        this.config.app = storedConfigObject.app
+        this.config.api = storedConfig.api
+        this.config.app = storedConfig.app
       } else {
-        this.config = storedConfigObject
+        this.config = storedConfig
       }
       deferred.resolve(this.config)
       return deferred.promise
@@ -25,6 +29,19 @@ export default class ConfigLoaderService {
       return this._load()
     }
   }
+
+  _getStoredConfig() {
+    let storedConfig = this.$window.localStorage.getItem('rt-config')
+    if(storedConfig) {
+      return JSON.parse(storedConfig)
+    }
+    return null
+  }
+
+  _setStoredConfig() {
+    this.$window.localStorage.setItem('rt-config', JSON.stringify(this.config))
+  }
+
   save (config) {
     let deferred = this.$q.defer()
     this.config = config
@@ -33,7 +50,7 @@ export default class ConfigLoaderService {
         config[key] = parseInt(config[key])
       }
     }
-    this.$window.localStorage.setItem('rt-config', JSON.stringify(config))
+    this._setStoredConfig()
     deferred.resolve(this.config)
     return deferred.promise
   }
@@ -44,6 +61,7 @@ export default class ConfigLoaderService {
       })
       .then(response => {
         this.config = response.data
+        this._setStoredConfig()
         if (this.config.api.host === true) {
           this.config.api.host = this.$window.location.hostname
         }
