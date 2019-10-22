1afb5b73d9b1ef6e0546903ad4b4616032ade4e2
James Gilmore <jamesg1381@gmail.com>
2019-10-22 20:31:44 +0100

    Limit to Global English

--- src/rtapp/services/language/language.service.js
+++ src/rtapp/services/language/language.service.js
@@ -14,61 +14,6 @@ export default class LanguageService {
           icon: emojione.shortnameToImage(':united_nations:'),
           name: 'Global English',
           default: true
-        },
-        {
-          code: 'en-US',
-          icon: emojione.shortnameToImage(':flag_us:'),
-          name: 'American English',
-        },
-        {
-          code: 'en-GB',
-          icon: emojione.shortnameToImage(':flag_gb:'),
-          name: 'British English'
-        },
-        {
-          code: 'en-AU',
-          icon: emojione.shortnameToImage(':flag_au:'),
-          name: 'Australian English'
-        },
-        {
-          code: 'it',
-          icon: emojione.shortnameToImage(':flag_it:'),
-          name: 'Italian'
-        },
-        {
-          code: 'nl',
-          icon: emojione.shortnameToImage(':flag_nl:'),
-          name: 'Dutch'
-        },
-        {
-          code: 'de',
-          icon: emojione.shortnameToImage(':flag_de:'),
-          name: 'German'
-        },
-        {
-          code: 'ja',
-          icon: emojione.shortnameToImage(':flag_jp:'),
-          name: 'Japanese'
-        },
-        {
-          code: 'es',
-          icon: emojione.shortnameToImage(':flag_es:'),
-          name: 'Spanish'
-        },
-        {
-          code: 'fr',
-          icon: emojione.shortnameToImage(':flag_fr:'),
-          name: 'French'
-        },
-        {
-          code: 'ru',
-          icon: emojione.shortnameToImage(':flag_ru:'),
-          name: 'Russian'
-        },
-        {
-          code: 'sv',
-          icon: emojione.shortnameToImage(':flag_se:'),
-          name: 'Swedish'
         }
       ])
     }).then(languages => {
